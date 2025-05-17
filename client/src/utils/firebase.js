4e5f61a6acaf24669f8d9fad503dcb4ef4c54150
ITACHI <vkforeverr143@gmail.com>
2025-05-17 09:30:01 +0530

    Add files via upload

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqx1N_07gTcWOGzfQUZJoszlvwubfY3Js",
+  authDomain: "taskmanagemet-19.firebaseapp.com",
+  projectId: "taskmanagemet-19",
+  storageBucket: "taskmanagemet-19.firebasestorage.app",
+  messagingSenderId: "959650015070",
+  appId: "1:959650015070:web:6169c9c6784dfbd6c0d1cf",
+  measurementId: "G-GFDDSQJGKK"
+};
+
+export const app = initializeApp(firebaseConfig);
+
